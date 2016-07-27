61ccdfb74b42a4223f40f4c5783e7bf06884211a
calvinm <Calvin.Mundy@derivco.co.za>
2016-07-27 08:44:07 +0200

    created two static functions: one to retrieve pure angle between two points (more of a direction) and another to get the angle of a sprite projectile depending on the start direction that it faces.
    Moved the functions into a class

--- coord.js
+++ coord.js
@@ -1,66 +1,102 @@
-/***********************************************************************************
-getPointToPointAngle
+class Coord {
 
-  returns an object
-   {
-     degrees: val,
-     radians: val
-   }
-  @param {int} initAngle
-     The angle that the sprite starts at. Pointing up is 0°
-     ▲ - 0°
-     ▼ - 180°
-     ► - 90°
-     ◄ - 270°
-  @param {int} x1
-    start x pos
-  @param {int} y1
-    start y pos
-  @param {int} x2
-    destination x pos
-  @param {int} y2
-    destination y pos
-  @param {bool} invertYAxis
-    Set to true if y increases going down as it does on canvas
-************************************************************************************/
-function getPointToPointAngle(initAngle, x1, y1, x2, y2, invertYAxis) {
-  var radAngle = Math.atan2(x2 - x1, y2 - y1);
-  var degrees = radAngle * (180 / Math.PI);
+  /***********************************************************************************
+  getPointToPointAngle
 
-  var adjustedAngle = degrees;
-  if (degrees < 0) {
-    adjustedAngle = 360 + degrees;
-  }
+    returns object
+     {
+       degrees: val,
+       radians: val
+     }
 
-  if (invertYAxis) {
-    //invert
-    console.log('angle before inversion: ' + adjustedAngle);
-    if (adjustedAngle < 180) {
-      adjustedAngle = 180 - adjustedAngle;
-    }
-    else if (adjustedAngle > 180 && adjustedAngle <360) {
-      adjustedAngle = (360 - adjustedAngle) + 180;
+    @param {int} x1
+      start x pos
+    @param {int} y1
+      start y pos
+    @param {int} x2
+      destination x pos
+    @param {int} y2
+      destination y pos
+    @param {bool} invertYAxis
+      Set to true if y increases going down as it does on canvas
+  ************************************************************************************/
+  static getPointToPointAngle(x1, y1, x2, y2, invertYAxis) {
+    var radAngle = Math.atan2(x2 - x1, y2 - y1);
+    var degrees = radAngle * (180 / Math.PI);
+
+    var adjustedAngle = degrees;
+    if (degrees < 0) {
+      adjustedAngle = 360 + degrees;
     }
 
-    console.log('inverted angle: ' + adjustedAngle);
-  }
-
-
-  //adjust for initial angle
-  if (initAngle !== 0) {
-    adjustedAngle = adjustedAngle - initAngle;
-    if (adjustedAngle < 0) {
-      adjustedAngle = 360 + adjustedAngle;
+    if (invertYAxis) {
+      //invert
+      //console.log('angle before inversion: ' + adjustedAngle);
+      if (adjustedAngle < 180) {
+        adjustedAngle = 180 - adjustedAngle;
+      }
+      else if (adjustedAngle > 180 && adjustedAngle <360) {
+        adjustedAngle = (360 - adjustedAngle) + 180;
+      }
+      //console.log('inverted angle: ' + adjustedAngle);
     }
-  }
-  console.log('adjusted angle from initial angle: ' + adjustedAngle);
 
-  //return object with degrees and radians
-  return {degrees: adjustedAngle, radians: adjustedAngle * (Math.PI / 180)}
+    //return object with degrees and radians
+    return {degrees: adjustedAngle, radians: adjustedAngle * (Math.PI / 180)}
+  }
+
+
+  /***********************************************************************************
+  getSpriteProjectileAngle
+
+    returns object
+     {
+       degrees: val,
+       radians: val
+     }
+
+    @param {int} initAngle
+       The angle that the sprite starts at. Pointing up is 0°
+       ▲ - 0°
+       ▼ - 180°
+       ► - 90°
+       ◄ - 270°
+    @param {int} x1
+      start x pos
+    @param {int} y1
+      start y pos
+    @param {int} x2
+      destination x pos
+    @param {int} y2
+      destination y pos
+    @param {bool} invertYAxis
+      Set to true if y increases going down as it does on canvas
+  ************************************************************************************/
+  static getSpriteProjectileAngle(initAngle, x1, y1, x2, y2, invertYAxis) {
+    let pointToPointAngle = Coord.getPointToPointAngle(x1, y1, x2, y2, invertYAxis).degrees;
+
+    //adjust for initial angle
+    if (initAngle !== 0) {
+      pointToPointAngle = pointToPointAngle - initAngle;
+      if (pointToPointAngle < 0) {
+        pointToPointAngle = 360 + pointToPointAngle;
+      }
+    }
+    //console.log('adjusted angle from initial angle: ' + pointToPointAngle);
+
+    //return object with degrees and radians
+    return {degrees: pointToPointAngle, radians: pointToPointAngle * (Math.PI / 180)}
+  }
+
 }
 
 
 
-//test
-var angle = getPointToPointAngle(90, 2, 1, 1, 0, true);
-console.log(angle)
+
+
+
+//TESTING
+var ptpAngle = Coord.getPointToPointAngle(2, 1, 1, 0, true);
+var spriteProjectileAngle = Coord.getSpriteProjectileAngle(90, 2, 1, 1, 0, true)
+console.log(ptpAngle);
+console.log(spriteProjectileAngle);
